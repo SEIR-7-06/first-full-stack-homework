611641b35dfd373bf9304a8879d999d815bf4891
erininabox <erininabox@gmail.com>
2021-08-04 14:53:05 -0700

    added essential file structure,

---
+++ models/planets.js
@@ -0,0 +1,58 @@
+const planets = [
+    {
+        name: 'Mercury',
+        official: true,
+        type: 'terrestrial planet',
+        lengthOfDay: 4222.6,
+    },
+    {
+        name: 'Venus',
+        official: true,
+        type: 'terrestrial planet',
+        lengthOfDay: 2802.0
+    },
+    {
+        name: 'Earth',
+        official: true,
+        type: 'terrestrial planet',
+        lengthOfDay: 24.0,
+    },
+    {
+        name: 'Mars',
+        official: true,
+        type: 'terrestrial planet',
+        lengthOfDay: 708.7,
+    },
+    {
+        name: 'Jupiter',
+        official: true,
+        type: 'gas giant',
+        lengthOfDay: 9.9,
+    },
+    {
+        name: 'Saturn',
+        official: true,
+        type: 'gas giant',
+        lengthOfDay: 10.7,
+    },
+    {
+        name: 'Uranus',
+        official: true,
+        type: 'gas giant',
+        lengthOfDay: 17.2,
+    },
+    {
+        name: 'Neptune',
+        official: true,
+        type: 'gas giant',
+        lengthOfDay: 16.1,
+    },
+    {
+        name: 'Pluto',
+        official: false,
+        type: 'dwarf planet',
+        lengthOfDay: 153.3,
+    }
+];
+
+module.exports = planets;
