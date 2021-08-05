8e356d2426369343e84e883af54eb038e2ef5fd0
erininabox <erininabox@gmail.com>
2021-08-04 19:52:03 -0700

    set up index.js in models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,26 @@
 // Set up connection to MongoDB
 
+// Set up dependency
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://localhost:27018/planetsdb'
+// Get the address to the Mongo database
+const connectionString = 'mongodb://localhost:27018/planetsdb';
+
+// Fire off the connection to the Mongo database
+mongoose.connect(connectionString, {
+    useNewUrlParse: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+// Listen for when we connect. Call back the function once connected.
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${connectionString}`);
+});
+
+// Export the model from this file.
+// This way the index.js file is our entry point for all of our data needs.
+module.exports = {
+    Planet: require('./Planet.js'),
+};
