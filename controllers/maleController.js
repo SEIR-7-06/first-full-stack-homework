77b3338431412d7b22f45ff76c9e07965f688634
nndeto <n_ndeto@yahoo.com>
2021-08-06 19:42:15 -0400

    coded edit route

--- controllers/maleController.js
+++ controllers/maleController.js
@@ -5,7 +5,7 @@ const router = express.Router();
 //stimulating a mongodb database environment
 const db = require('../models/index.js')
 
-//Index Route --> needs ejs
+//Index Route --> DONE //needs ejs
 router.get('/', (req, res) => {
     db.Male.find({}, (err, allMales) => {
         if (err) return console.log(err);
@@ -15,17 +15,22 @@ router.get('/', (req, res) => {
     })
 })
 
-//New Route --> needs ejs, form and sends form to create route 
+//New Route -->  DONE //needs ejs, form and sends form to create route 
 router.get('/new', (req, res) => {
     res.render('new.ejs')
 })
 
-//Show Route --> needs ejs
+//Show Route --> DONE //needs ejs
 router.get('/:maleId', (req, res) => {
-    res.render('show.ejs')
+    db.Male.findById(req.params.maleId, (err, singleMale) => {
+        if (err) return console.log(err);
+        res.render('show.ejs', {
+            singleMale: singleMale
+        })
+    })
 })
 
-//Create Route
+//Create Route --> DONE
 router.post('/', (req, res) => {
     // console.log("you reached the create route")
     //we do this so it can show up in the data regardless of on or off status
@@ -43,16 +48,22 @@ router.post('/', (req, res) => {
 
 //Edit Route --> needs ejs, form and sends form to update route
 router.get('/:maleId/edit', (req, res) => {
-    res.render('update.ejs')
+    maleId = req.params.maleId
+    db.Male.findById(maleId, (err, foundMale) => {
+        if (err) return console.log(err)
+        res.render('update.ejs', {
+            oneMale: foundMale
+        })
+    })
 })
 
-//Update Route --> 
+//Update Route --> updates the db data
 router.put('/:maleId', (req,res) => {
     console.log("you've hit the update route")
     console.log(req.body);
 })
 
-//Delete Route
+//Delete Route --> DONE
 router.delete('/:maleId', (req, res) => {
     const maleId = req.params.maleId;
     db.Male.findByIdAndDelete(maleId, (err) => {
