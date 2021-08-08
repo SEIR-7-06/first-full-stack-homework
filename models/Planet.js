b74367191ed2b7bb982cce22cda70ab54a26b9bf
erininabox <erininabox@gmail.com>
2021-08-07 18:06:02 -0700

    added data via mongo

--- models/Planet.js
+++ models/Planet.js
@@ -17,60 +17,60 @@ module.exports = Planet;
 // Data for the planets for later
 
 // const planets = [
-//     {
-//         name: 'Mercury',
-//         official: true,
-//         type: 'terrestrial planet',
-//         lengthOfDay: 4222.6,
-//     },
-//     {
-//         name: 'Venus',
-//         official: true,
-//         type: 'terrestrial planet',
-//         lengthOfDay: 2802.0
-//     },
-//     {
-//         name: 'Earth',
-//         official: true,
-//         type: 'terrestrial planet',
-//         lengthOfDay: 24.0,
-//     },
-//     {
-//         name: 'Mars',
-//         official: true,
-//         type: 'terrestrial planet',
-//         lengthOfDay: 708.7,
-//     },
-//     {
-//         name: 'Jupiter',
-//         official: true,
-//         type: 'gas giant',
-//         lengthOfDay: 9.9,
-//     },
-//     {
-//         name: 'Saturn',
-//         official: true,
-//         type: 'gas giant',
-//         lengthOfDay: 10.7,
-//     },
-//     {
-//         name: 'Uranus',
-//         official: true,
-//         type: 'gas giant',
-//         lengthOfDay: 17.2,
-//     },
-//     {
-//         name: 'Neptune',
-//         official: true,
-//         type: 'gas giant',
-//         lengthOfDay: 16.1,
-//     },
-//     {
-//         name: 'Pluto',
-//         official: false,
-//         type: 'dwarf planet',
-//         lengthOfDay: 153.3,
-//     }
+    // {
+    //     planetName: 'Mercury',
+    //     official: true,
+    //     planetType: 'terrestrial planet',
+    //     lengthOfDay: 4222.6,
+    // },
+    // {
+    //     planetName: 'Venus',
+    //     official: true,
+    //     planetType: 'terrestrial planet',
+    //     lengthOfDay: 2802.0
+    // },
+    // {
+    //     planetName: 'Earth',
+    //     official: true,
+    //     planetType: 'terrestrial planet',
+    //     lengthOfDay: 24.0,
+    // },
+    // {
+    //     planetName: 'Mars',
+    //     official: true,
+    //     planetType: 'terrestrial planet',
+    //     lengthOfDay: 708.7,
+    // },
+    // {
+    //     planetName: 'Jupiter',
+    //     official: true,
+    //     planetType: 'gas giant',
+    //     lengthOfDay: 9.9,
+    // },
+    // {
+    //     planetName: 'Saturn',
+    //     official: true,
+    //     planetType: 'gas giant',
+    //     lengthOfDay: 10.7,
+    // },
+    // {
+    //     planetName: 'Uranus',
+    //     official: true,
+    //     planetType: 'gas giant',
+    //     lengthOfDay: 17.2,
+    // },
+    // {
+    //     planetName: 'Neptune',
+    //     official: true,
+    //     planetType: 'gas giant',
+    //     lengthOfDay: 16.1,
+    // },
+    // {
+    //     planetName: 'Pluto',
+    //     official: false,
+    //     planetType: 'dwarf planet',
+    //     lengthOfDay: 153.3,
+    // }
 // ];
 
 // module.exports = planets;
