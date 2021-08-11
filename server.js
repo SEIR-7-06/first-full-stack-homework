f872283139de4ee1198b06429a3e0cb0d23171c1
marshallwatrous <marshallwatrous95@gmail.com>
2021-08-11 15:12:30 -0700

    Edits server.js and adds views folder

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ const express = require("express");
 const methodOverride = require("method-override");
 const rowdyLogger = require("rowdy-logger");
 const app = express();
-const controller = require("./controller/controller");
+const controller = require("./controller/controller.js");
 const port = 4000;
 const rowdy = rowdyLogger.begin(app);
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
-app.use(methodOverride("_method"))
-app.use("express.urlEncoded({extended:false"}));
+app.use(express.urlEncoded({ extended: false }))
 app.use("/names", controller);
-app.listen(port () => {
-    rowdy.results()
+app.get("/dragon", (req, res) => {
+    res.send("This is not a name!");
 })
 
+app.get("/", (req, res) => {
+    res.send("This is my Names Website!");
+})
 
-
-modules. exports(app);
+app.listen(port () => {
+    console.log(`Your server us running on port: ${port}`);
+    rowdy.print()
+});
