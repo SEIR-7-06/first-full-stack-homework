fbf188c86cad55e9cc4cd06ca0168195955f0108
marshallwatrous <marshallwatrous95@gmail.com>
2021-08-11 12:50:08 -0700

    Adds more to server.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const app = express;
+const rowdyLogger = require("rowdy-logger");
+const methodOverride = require("method-override");
+const controller = require("./controllers/controller");
+const app = express();
 const port = 4000;
-
+const rowdy = rowdyLogger.begin(app);
+app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
 modules. exports(app);
