5133e9a667fef510fe19b81322d6fd25e87fbb50
marshallwatrous <marshallwatrous95@gmail.com>
2021-08-11 14:43:45 -0700

    Adds and completes nameModel.js

---
+++  models/nameModel.js
@@ -0,0 +1,17 @@
+const names = [
+    {
+        letters: 8,
+        firstName: "Marshall",
+        startsWithAConsonant: true
+    },
+    {
+        letters: 4,
+        firstName: "Alex",
+        startsWithAConsonant: false
+    },
+    {
+        letters: 3,
+        firstName: "Bob",
+        startsWithAConsonant: true
+    }
+];
