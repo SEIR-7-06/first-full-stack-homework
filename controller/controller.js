87a5b0f4c77aa39b7c1b116bb25d52f5de674347
marshallwatrous <marshallwatrous95@gmail.com>
2021-08-11 13:48:17 -0700

    Adds to controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -1,3 +1,23 @@
 const express = require("express");
-const dataBase = require(".../models/server.js");
+const dataBase = require(".../models/index.js");
 const router = express.Router();
+router.get("/", (req, res) => {
+    dataBase.Name.find({}, (err, allNames) => {
+        if(err) return console.log(err);
+
+        res.render("index.ejs", { allNames: allNames})
+    })
+})
+
+router.get("/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+router.get("/:nameId", (req, res) => {
+    dataBase.Name.findById(req.params.nameId, (err, foundName) => {
+        if (err) return console.log(err);
+        res.render("show.ejs"), { oneFruit: foundFruit })
+    })
+})
+
+router.post
