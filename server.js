68d13a2afe65f8f3aedae5a4096e9845994919df
marshallwatrous <marshallwatrous95@gmail.com>
2021-08-11 13:03:13 -0700

    Adds more to server.js

--- server.js
+++ server.js
@@ -7,4 +7,11 @@ const port = 4000;
 const rowdy = rowdyLogger.begin(app);
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
+app.use(methodOverride("_method"))
+app.use("express.urlEncoded({extended:false"}));
+app.use("/names", controller);
+
+
+
+
 modules. exports(app);
