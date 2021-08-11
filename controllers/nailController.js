3459b774db0ebd63f1c5b16a61b89897f8c58e42
stephanyburden <stephanyburden@gmail.com>
2021-08-10 23:18:08 -0700

    completed full stack hw

--- controllers/nailController.js
+++ controllers/nailController.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const nails = require('../models/NailsModel.js')
+//const nails = require('../models/NailsModel.js')
+const db = require('../models/index.js');
+
 
 
 // Index Route //
 router.get('/', (req, res) => {
-    res.render('index.ejs', {
-        allNails: nails
+    db.Nails.find({}, (err, allNails) => {
+        if (err) return console.log(err);
+        
+        
+        res.render('index.ejs', {
+            allNails: allNails
+    });
     })
 })
 
@@ -16,35 +23,58 @@ router.get('/new', (req, res) => {
 })
 
 // Show Route // 
-router.get('/:nailIndex', (req, res) => {
-    res.render('show.ejs', {
-        oneNail: nails[req.params.nailIndex]
+router.get('/:nailId', (req, res) => {
+    db.Nails.findById(req.params.nailId, (err, foundNail) => {
+        if (err) return console.log(err);
+        
+        
+        res.render('show.ejs', { oneNail: foundNail })
     })
 })
 
 // Create Route //
 router.post('/', (req, res) => {
     //console.log(req.body)
-    nails.push(req.body);
-    res.redirect('/nails');
-    //console.log('hey you reach the create route')
-
+    db.Nails.create(req.body, (err, createdNails) => {
+        if (err) return console.log(err);
+        
+        
+        res.redirect('/nails');
+    });
 })
 
 
 // Edit Route //
-router.get('/:nailsIndex/edit', (req, res) => {
-    res.render('edit.ejs', {
-        oneNail: nails[req.params.nailsIndex],
-        index: req.params.nailsIndex
+router.get('/:nailId/edit', (req, res) => {
+    db.Nails.findById(req.params.nailId, (err, foundNail) => {
+        if (err) return console.log(err);
+
+        res.render('edit.ejs', { oneNail: foundNail });
     })
 })
 
 
+//////////     Edit && Update go hand in hand      //////////
 
 
+// Update Route //
+router.put('/:nailId', (req, res) => {
+    db.Nails.findByIdAndUpdate(req.params.nailId, req.body, (err, updateNail) => {
+        if (err) return console.log(err);
+        
+        res.redirect('/nails/' + req.params.nailId);
+    })
+})
 
 
+// Destroy Route // 
+router.delete('/:nailId',(req, res) => {
+    db.Nails.findByIdAndDelete(req.params.nailId, (err) => {
+        if (err) return console.log(err);
+        res.redirect('/nails');
+    })
+    //console.log("Ay yo mama")
+})
 
 
 
--- models/index.js
+++ models/index.js
@@ -1,1 +1,23 @@
-// Index.js connects our server to MongoDB
+// Connect to our database
+const mongoose = require('mongoose');
+
+
+// Getting the address to the Mongo Database
+const connectionString = 'mongodb://localhost:27017/nailsdb';
+
+// Fireing off Connection to MongoDB & deactives those mongoose deprication warnings
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+
+  // Listen for connection and call a call back function when we do connect //
+  mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${connectionString} 🤍`);
+  });
+
+module.exports = {
+    Nails: require('./nails.js')
+}
